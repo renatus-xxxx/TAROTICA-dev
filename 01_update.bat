956c75df710951c1d15c372233f253d499fdb24c
nnkgw <148290899+renatus-xxxx@users.noreply.github.com>
2024-11-03 16:46:20 +0900

    set path

--- 01_update.bat
+++ 01_update.bat
@@ -1,7 +1,41 @@
 @echo off
 cd /d %~dp0
-bin\file2sav\file2sav.exe "C:\Program Files (x86)\Steam\steamapps\common\TAROTICA VOO DOO\180621\TaroticaVooDoo_disk00.sav" @RESPONSE.TXT
-taskkill /F /IM "TaroticaVooDoo.exe"
-start "" "C:\Program Files (x86)\Steam\steamapps\common\TAROTICA VOO DOO\TaroticaVooDoo.exe"
-taskkill /F /IM "savlist.exe"
-start "" "bin\savlist\savlist.exe" "C:\Program Files (x86)\Steam\steamapps\common\TAROTICA VOO DOO\180621\TaroticaVooDoo_disk00.sav"
+
+set "FILE2SAV_PATH=bin\file2sav\file2sav.exe"
+if not exist "%FILE2SAV_PATH%" (
+  echo Please run "00_setup.bat" to set up
+  pause
+  exit /b 1
+)
+
+set "SAVLIST_PATH=bin\savlist\savlist.exe"
+if not exist "%SAVLIST_PATH%" (
+  echo Please run "00_setup.bat" to set up
+  pause
+  exit /b 1
+)
+
+set "SAV_PATH=C:\Program Files (x86)\Steam\steamapps\common\TAROTICA VOO DOO\180621\TaroticaVooDoo_disk00.sav"
+set "EXE_PATH=C:\Program Files (x86)\Steam\steamapps\common\TAROTICA VOO DOO\TaroticaVooDoo.exe"
+
+if exist "%EXE_PATH%" (
+  if exist "%SAV_PATH%" (
+    bin\file2sav\file2sav.exe "%SAV_PATH%" @RESPONSE.TXT
+    taskkill /F /IM "TaroticaVooDoo.exe" >nul 2>&1
+    start "" "%EXE_PATH%"
+    taskkill /F /IM "savlist.exe" >nul 2>&1
+    start "" "bin\savlist\savlist.exe" "%SAV_PATH%"
+    exit /b 0
+  ) else (
+    echo Modify the bat file to set the full path to "TaroticaVooDoo_disk00.sav"
+    echo (—á:"C:\Program Files (x86)\Steam\steamapps\common\TAROTICA VOO DOO\180621\TaroticaVooDoo_disk00.sav")
+    pause
+    exit /b 1
+  )
+) else (
+  echo Modify the bat file to set the full path to "TaroticaVooDoo.exe"
+  echo (—á:"C:\Program Files (x86)\Steam\steamapps\common\TAROTICA VOO DOO\TaroticaVooDoo.exe")
+  pause
+  exit /b 1
+)
+
